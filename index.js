6dc1ceb452294b8d1489bdc556181c74464c8c65
ObamaBolsa <daniel.alegria.lino@gmail.com>
2022-03-26 02:58:31 -0600

    lino

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
 const express = require('express')
 
+const session = require('express-session');
+
 const app = express()
 
 const router = require('./router')
 
-app.use(express.urlencoded({extended:false}))
+const morgan = require('morgan');
+
+const passport = require('passport')
+
+//Inicialiacions
+require('./database')
+require('./passport/local-auth')
+
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use(express.static('public'))
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
+
 // app.set('views','views')
 
+
+
+app.set('port', process.env.PORT || 3000)
+
+//middle ware
+app.use(morgan('dev'))
+app.use(passport.initialize());
+app.use(session({
+    secret: 'mysecretsession',
+    resave: false,
+    saveUninitialized: false
+}));
+
+//Manage loguin datos ((;
+app.use(express.urlencoded({ extended: false }));
+
+
+//Rutas
 app.use('/', router)
-app.listen(3000);
+app.listen(app.get('port'), () => {
+    console.log('listening on port' + app.get('port'))
+
+});
 
 //Exporting the app
 module.exports = app
--- router.js
+++ router.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('./controllers/userController')
+const passport = require('passport');
 
 router.get('/', userController.getPaginaAcceso);
 // router.get('/', userController.getPaginaInicio);
 router.get('/login', userController.login);
+
+router.get('/signup', (req, res, next) => {
+    res.render('signup')
+});
+
+router.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    passReqToCallback: true
+}));
+
+
 
 module.exports = router
