6302579d7ada4e54ba93d04e9a73d67d52b3ed2f
Alberto Beltran <albertobeltran@protonmail.com>
2022-03-26 02:42:31 -0600

    agregue pagina de inicio y sus estilos

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,33 @@
 const User = require('../models/User');
 
+let css_links = [];
+
 function getPaginaAcceso(req, res) {
-    res.render('acceso');
+    css_links.push('<link rel="stylesheet" href="estilos/menu.css">');
+    css_links.push('<link rel="stylesheet" href="estilos/acceso.css">');
+    res.render('acceso', { css_links: css_links });
 }
 
-// function getPaginaInicio(req, res) {
-//     res.render('');
-// }
+function getPaginaInicio(req, res) {
+    css_links.push('<link rel="stylesheet" href="estilos/menu.css">');
+    css_links.push('<link rel="stylesheet" href="estilos/inicio.css">');
+    res.render('inicio', { css_links: css_links });
+}
 
 function login(req, res) {
-    res.render('login');
+    css_links.push('<link rel="stylesheet" href="estilos/menu.css">');
+    css_links.push('<link rel="stylesheet" href="estilos/login-registro.css">');
+    res.render('login', { css_links: css_links });
 }
 
 function registro(req, res) {
-    res.render('registro');
+    css_links.push('<link rel="stylesheet" href="estilos/menu.css">');
+    css_links.push('<link rel="stylesheet" href="estilos/login-registro.css">');
+    res.render('registro', { css_links: css_links });
 }
 
 
 exports.getPaginaAcceso = getPaginaAcceso;
+exports.getPaginaInicio = getPaginaInicio;
 exports.login = login;
 exports.registro = registro;
--- router.js
+++ router.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const userController = require('./controllers/userController')
 
 router.get('/', userController.getPaginaAcceso);
-// router.get('/', userController.getPaginaInicio);
+router.get('/inicio', userController.getPaginaInicio);
 router.get('/login', userController.login);
 router.get('/registro', userController.registro);
 
